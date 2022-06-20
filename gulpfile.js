33a50d97cabe6ece5e5d079d258b3ff60b29c672
Yuriy Panchenko <yuriy.panchenko@p2h.com>
2022-06-20 11:48:07 +0300

    'update'

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const { src, dest, series, parallel, watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
+const imagemin = require('gulp-imagemin');
 var browserSync = require('browser-sync').create();
 
 function server() {
@@ -11,10 +12,16 @@ function server() {
     });
 }
 
+function fonts() {
+    return src('./dev/fonts/*')
+        .pipe(dest('./public/fonts/'))
+        .pipe(browserSync.stream());
+}
+
 function copy() {
     return src('./dev/js/**/*.js')
         .pipe(dest('./public/js/'))
-        .pipe(browserSync.stream())
+        .pipe(browserSync.stream());
 }
 
 function html() {
@@ -26,20 +33,42 @@ function html() {
 function scss() {
     return src('./dev/scss/**/*.scss')
         .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
+        .pipe(sass(
+            //{ outputStyle: 'compressed' }
+        ).on('error', sass.logError))
         .pipe(sourcemaps.write())
         .pipe(dest('./public/css/'))
-        .pipe(browserSync.stream())
+        .pipe(browserSync.stream());
+}
+
+function images() {
+    return src('./dev/images/**/*')
+        .pipe(imagemin([
+            imagemin.gifsicle({ interlaced: true }),
+            imagemin.mozjpeg({ quality: 75, progressive: true }),
+            imagemin.optipng({ optimizationLevel: 5 }),
+            imagemin.svgo({
+                plugins: [
+                    { removeViewBox: true },
+                    { cleanupIDs: false }
+                ]
+            })]))
+        .pipe(dest('./public/images/'))
+        .pipe(browserSync.stream());
 }
 
 function watching() {
     watch('./dev/scss/**/*.scss', scss);
     watch('./dev/js/**/*.js', copy);
     watch('./dev/*.html', html);
+    watch('./dev/images/**/*', imagemin);
+    watch('./dev/fonts/*', fonts);
 };
 
+exports.fonts = fonts;
+exports.images = images;
 exports.watching = watching;
 exports.server = server;
 exports.html = html;
 
-exports.default = parallel(html, server, watching);
+exports.default = parallel(html, images, fonts, server, watching);
